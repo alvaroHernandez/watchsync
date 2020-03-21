f7a8ed21f6f0a32884d9b74090be3c828099b9a1
joanca <jcaravenae@gmail.com>
2020-03-21 20:03:30 -0300

    [api] adding cuevana movie api

---
+++ pages/api/movie/[mid].js
@@ -0,0 +1,39 @@
+import fetch from 'node-fetch'
+
+const CUEVANA_API = "https://api.cuevana3.io/stream/plugins/gkpluginsphp.php"
+
+export default async (req, res) => {
+	const {
+    query: { mid },
+  } = req
+
+  res.statusCode = 200
+  res.setHeader('Content-Type', 'application/json')
+
+  try {
+	  const movie = await getCuevanaMovie(mid) 	
+
+	  res.json(movie)
+	} catch(error) {
+		res.json({
+			error
+		})
+	}
+}
+
+const getCuevanaMovie = async (fileId) => {
+	const options = {
+		method: 'POST',
+		headers: {
+    	'Content-Type': 'application/x-www-form-urlencoded',
+    	'Accept-Encoding': 'gzip, deflate, br',
+    	'Origin': 'https://api.cuevana3.io'
+    },
+		body: `link=${fileId}`
+	}
+
+	const result = await fetch(CUEVANA_API, options)
+	const json = await result.json()
+
+	return json
+}
