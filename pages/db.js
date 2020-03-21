d06fde027bd13f679b27910cc3ab5b3fd6303959
alvaroHernandez <alvaro.hernandezi@alumnos.usm.cl>
2020-03-21 20:00:22 -0300

    revert firestore

--- pages/db.js
+++
@@ -1,7 +0,0 @@
-const admin = require('firebase-admin');
-admin.initializeApp({
-    credential: admin.credential.cert(JSON.parse(process.env.GCLOUD_CREDENTIALS))
-});
-
-const db = admin.firestore();
-export default db
