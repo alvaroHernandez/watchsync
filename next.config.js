3575b0d61608d743b9066434bc66f2c5929142f8
alvaroHernandez <alvaro.hernandezi@alumnos.usm.cl>
2020-03-21 19:03:37 -0300

    fixing webpack modules error

--- next.config.js
+++ next.config.js
@@ -1,5 +1,17 @@
-exports.default = {
-    env: {
-        GCLOUD_CREDENTIALS: process.env.GCLOUD_CREDENTIALS
+module.exports = {
+    webpack: (config) => {
+        // Fixes npm packages that depend on certain modules
+        config.node = {
+            ...config.node,
+            http2: 'empty',
+            fs: 'empty',
+            child_process : 'empty',
+            net : 'empty',
+            tls: 'empty',
+            dns: 'empty',
+
+        }
+
+        return config
     }
-};
+}
