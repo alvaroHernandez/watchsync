125e6d12e794387afea4ff854b246b1c50fde494
Daniela Mora <dmora@thoughtworks.com>
2020-03-21 20:02:27 -0300

    add styles for MovieList

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,14 +1,14 @@
 import Link from 'next/link'
-import './MovieList.module.scss'
+import styles from './MovieList.module.scss'
 
 const MovieList = () =>(
-    <div className="movie-item" >
-        <ul>
-            <li> <Link href="/movie/1"><a>Alfa</a></Link> </li>
-            <li> <Link href="/movie/2"><a>Beta</a></Link> </li>
-            <li> <Link href="/movie/3"><a>Gama</a></Link> </li>
-            <li> <Link href="/movie/4"><a>Delta</a></Link> </li>
-        </ul>
+    <div className={styles.movieItem} >
+        <div className="information">
+            <span className={styles.title}> <Link href="/movie/1"><a>Alfa</a></Link> </span>
+            <span className={styles.title}> <Link href="/movie/2"><a>Beta</a></Link> </span>
+            <span className={styles.title}> <Link href="/movie/3"><a>Gama</a></Link> </span>
+            <span className={styles.title}> <Link href="/movie/4"><a>Delta</a></Link> </span>
+        </div>
     </div>  
 )
 
