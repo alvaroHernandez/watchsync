75c89fa2bf5045574c4b8db8bfb6a1e861972a6f
Cristina Cares Unquén <cristina.cares@thoughtworks.com>
2020-03-21 23:52:05 -0300

    Change styles movie item

--- src/components/MovieItem/MovieItem.js
+++ src/components/MovieItem/MovieItem.js
@@ -3,6 +3,10 @@ import styles from './MovieItem.module.scss'
 
 const MovieItem = () =>(
     <div className={styles["movie-item"]} >
+        <div className={styles["image-container"]}>
+            <img alt="portada pelicula" className={styles.thumbnail}
+                 src="https://m.media-amazon.com/images/M/MV5BNGVjNWI4ZGUtNzE0MS00YTJmLWE0ZDctN2ZiYTk2YmI3NTYyXkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_.jpg"/>
+        </div>
         <div className={styles.information}>
             <span className={styles.title}> <Link href="/movie/1"><a>Alfalfa</a></Link> </span>
             {/*<span className={styles.title}> <Link href="/movie/2"><a>Beta</a></Link> </span>
--- src/components/MovieList/MovieList.js
+++ src/components/MovieList/MovieList.js
@@ -1,12 +1,11 @@
 import MovieItem from "../MovieItem/MovieItem";
 import styles from './MovieList.module.scss'
-import stylesItem from '../MovieItem/MovieItem.module.scss'
 
 const MovieList  = () =>  (
     <div className={styles.container}>
         <div className={styles.box}>
-            <div className={stylesItem.title}>
-                <h1>Titulo</h1>
+            <div>
+                <h1 className={styles.title}>Movies</h1>
             </div>
             <div>
                 <div className={styles["movies-scroll"]}>
