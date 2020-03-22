7017f9e94761d1a5cf83ea09aad4cd5830d5a334
alvaroHernandez <alvaro.hernandezi@alumnos.usm.cl>
2020-03-22 12:57:31 -0300

    logging erros in rooms api

--- pages/api/rooms/[roomId]/movie/[movieUrl].js
+++ pages/api/rooms/[roomId]/movie/[movieUrl].js
@@ -2,35 +2,41 @@ const db = require('../../../../../lib/db')
 const escape = require('sql-template-strings')
 
 export default async (req, res) => {
-    if (req.method === 'POST') {
-        const rooms = await db.query(escape`
+    try{
+        if (req.method === 'POST') {
+            const rooms = await db.query(escape`
           SELECT uuid from rooms
           WHERE uuid = ${req.query.roomId}
         `)
-        if(rooms .length === 0){
-            const rooms = await db.query(escape`
+            if(rooms .length === 0){
+                const rooms = await db.query(escape`
               INSERT INTO rooms (uuid,movieUrl) VALUES (${req.query.roomId},${req.query.movieUrl})
             `)
 
-            if(rooms.error){
-                res.status(500).json( { error : rooms.error } )
+                if(rooms.error){
+                    res.status(500).json( { error : rooms.error } )
+                }else{
+                    res.status(201).json({ rooms })
+                }
             }else{
-                res.status(201).json({ rooms })
+                res.status(200).json({ rooms })
             }
-        }else{
-            res.status(200).json({ rooms })
-        }
-    }else if (req.method === 'PUT') {
-        const rooms = await db.query(escape`
+        }else if (req.method === 'PUT') {
+            const rooms = await db.query(escape`
           UPDATE rooms set hostId = ${req.query.hostId}
           WHERE uuid = ${req.query.roomId}
         `)
-        res.status(201).json({ rooms })
-    }else if (req.method === 'GET') {
-        const rooms = await db.query(escape`
+            res.status(201).json({ rooms })
+        }else if (req.method === 'GET') {
+            const rooms = await db.query(escape`
           SELECT hostId from rooms
           WHERE uuid = ${req.query.roomId}
         `)
-        res.status(201).json(rooms[0])
+            res.status(201).json(rooms[0])
+        }
+
+    }catch(e){
+        console.log("Error in Rooms API: ", e);
+        res.status(500).json({error: e});
     }
 }
